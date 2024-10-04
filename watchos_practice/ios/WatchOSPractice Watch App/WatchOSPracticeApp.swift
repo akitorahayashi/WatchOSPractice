11ebf2653e8671c2c7b61daac13f966e166d23ca
林明虎 <hayashi20030703@gmail.com>
2024-10-04 18:25:44 +0900

    a

---
+++ watchos_practice/ios/WatchOSPractice Watch App/WatchOSPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchOSPracticeApp.swift
+//  WatchOSPractice Watch App
+//
+//  Created by 林 明虎 on 2024/10/03.
+//
+
+import SwiftUI
+
+@main
+struct WatchOSPractice_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
